dd49cf46a0e8bca5a7a3f312281d0d185238fef9
SW\yrlee2 <yrlee2@shinwon.com>
2022-10-05 16:11:52 +0900

    test: String 클래스에 대한 학습테스트를 추가하라

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,8 @@
 package study;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,5 +12,32 @@ public class StringTest {
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+    
+    @Test
+    void split1() {
+    	String[] actual = "1,2".split(",");
+    	assertThat(actual).contains("1", "2");
+    }
+    
+    @Test
+    void split2() {
+    	String[] actual = "1".split(",");
+    	assertThat(actual).containsExactly("1");
+    }
+    
+    @Test
+    void substring() {
+    	String actual = "(1,2)";
+    	String subString = actual.substring(1, actual.length() - 1);
+    	
+    	assertThat(subString).isEqualTo("1,2");
+    }
+    
+    @Test
+    @DisplayName("charAt() 메서드 범위 값을 벗어나면 StringIndexOutOfBoundsException이 발생한다")
+    void charAt() {
+    	String abc = "abc";
+    	assertThrows(StringIndexOutOfBoundsException.class, () -> abc.charAt(abc.length()));
     }
 }
